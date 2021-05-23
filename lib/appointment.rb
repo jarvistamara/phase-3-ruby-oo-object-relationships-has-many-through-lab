e99b806394c77e02931804427644da6a03a132bd
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-05-23 10:47:07 -0400

    commit tree 1468ba8e7064aeaf619a2d80f7daabdf08cfb068

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,43 @@
 
+class Appointment
+    attr_accessor :patient, :doctor, :date
+    @@all = []
+
+    def initialize(patient, doctor, date)
+    @patient = patiend
+    @doctor = doctor
+    @date = date
+    @@all << self
+    end
+
+    def self.all
+    @@all
+    end
+end
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    end
+    
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,20 @@
+class Song
+    attr_reader :artist, :genre
+    attr_accessor :name
+    @@all = []
 
+    def initialize(artist, genre, name)
+    @artist = artist
+    @genre = genre
+    @name = name
+    @@all << self
+    end
+
+    def self.all
+    @@all
+    end
+
+    def say_hi
+    puts 'hi'
+    end
+end
